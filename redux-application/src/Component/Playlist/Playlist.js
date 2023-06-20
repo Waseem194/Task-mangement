376e2575b3834acc9e6a48c792e829aa3003be0b
Waseem Amanat <prowaseem1994@gmail.com>
2023-06-20 16:37:02 +0500

    reduxapp

--- redux-application/src/Component/Playlist/Playlist.js
+++
@@ -1,8 +0,0 @@
-
-const Playlist=()=>{
-
-   const song={
-    
-   } 
-}
-export default Playlist;
--- redux-application/src/Component/Store/Store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from "redux";
-let Initial = {
-    song:[],
-   
-};
-let data = combineReducers({userSection, productSection});
-
-export let meraStore = createStore(data);
-
